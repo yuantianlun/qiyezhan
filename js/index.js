78cd36e156ffa701243927a36977e601538e5f52
yuantianlun <997795531@qq.com>
2018-01-22 13:38:52 +0800

    'a'

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    let banner=document.querySelector(".banner");
+    let lis=document.querySelectorAll(".banner ul li");
+    console.log(banner);
+    console.log(lis);
+}
